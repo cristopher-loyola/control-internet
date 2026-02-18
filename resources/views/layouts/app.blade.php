44b5a8642f07c8360d1b42e0531fcd5fab7164f5
cristo loyola <157190820+cristopher-loyola@users.noreply.github.com>
2026-02-18 16:00:22 -0600

    Aqui ya el icnono de la pestaña siempre tendra el logo, tambien la pestaña siempre dira Control internet

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,7 +5,8 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <title>{{ $title ?? config('app.name', 'Laravel') }}</title>
+        <title>{{ config('app.name', 'Control Internet') }}</title>
+        <link rel="icon" type="image/png" href="{{ asset('images/clogo2.png') }}">
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,8 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+        <title>{{ config('app.name', 'Control Internet') }}</title>
+        <link rel="icon" type="image/png" href="{{ asset('images/clogo2.png') }}">
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
